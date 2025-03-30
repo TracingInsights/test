cf6bc35a8fdddd691bf376e05178169772e5847f
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2025-03-30 21:20:18 +0530

    Update telQ.py

--- telQ.py
+++ telQ.py
@@ -46,7 +46,33 @@ class TelemetryExtractor:
     ):
         """Initialize the TelemetryExtractor."""
         self.year = year
-        self.events = events or ["Australian Grand Prix"]
+        self.events = events or [
+    # "Pre-Season Testing",
+    # "Australian Grand Prix",
+    'Chinese Grand Prix',
+    # 'Japanese Grand Prix',
+    # 'Bahrain Grand Prix',
+    # 'Saudi Arabian Grand Prix',
+    # 'Miami Grand Prix',
+    # "Emilia Romagna Grand Prix",
+    # 'Monaco Grand Prix',
+    # 'Spanish Grand Prix',
+    # 'Canadian Grand Prix',
+    # 'Austrian Grand Prix',
+    # 'British Grand Prix',
+    # 'Belgian Grand Prix',
+    # 'Hungarian Grand Prix',
+    # 'Dutch Grand Prix',
+    # 'Italian Grand Prix',
+    # 'Azerbaijan Grand Prix',
+    # 'Singapore Grand Prix',
+    # 'United States Grand Prix',
+    # 'Mexico City Grand Prix',
+    # 'São Paulo Grand Prix',
+    # 'Las Vegas Grand Prix',
+    # 'Qatar Grand Prix',
+    # 'Abu Dhabi Grand Prix',
+]
         self.sessions = sessions or ["Race"]
 
     def get_session(
@@ -107,7 +133,7 @@ class TelemetryExtractor:
             driver_laps["LapTime"] = driver_laps["LapTime"].apply(
                 lambda x: x.total_seconds() if hasattr(x, "total_seconds") else x
             )
-            driver_laps = driver_laps[driver_laps.LapTime.notnull()]
+            
 
             return {
                 "time": driver_laps["LapTime"].tolist(),
